661d4b0b34ae370c2eb795ea582df06d7a0dc2b5
Kevin Trotman <kevintrotman@icloud.com>
2023-09-08 12:05:09 -0400

    Adding CMake Setup to aid debugging/Compiling.

---
+++ build/CMakeFiles/Qube.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Qube.dir/qube.cpp.o"
+  "CMakeFiles/Qube.dir/qube.cpp.o.d"
+  "Qube"
+  "Qube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Qube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
