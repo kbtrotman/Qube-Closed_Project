8fc2f493685fe59ff7a4a9caeee036707dbce404
Kevin Trotman <ktrotman@dev01.kevinslab.com>
2023-09-27 00:12:42 -0400

    Setting up home location also.

---
+++ build/CMakeFiles/QubeFS.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ktrotman/c/Qube/q_dedupe.cpp" "/home/ktrotman/c/Qube/build/CMakeFiles/QubeFS.dir/q_dedupe.cpp.o"
+  "/home/ktrotman/c/Qube/q_fuse.cpp" "/home/ktrotman/c/Qube/build/CMakeFiles/QubeFS.dir/q_fuse.cpp.o"
+  "/home/ktrotman/c/Qube/qube.cpp" "/home/ktrotman/c/Qube/build/CMakeFiles/QubeFS.dir/qube.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/trotmkeb/c/Qube"
+  "/usr/include/fuse3"
+  "/usr/include/spdlog"
+  "/usr/include/pq"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/QubeFS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QubeFS.dir/q_dedupe.cpp.o"
+  "CMakeFiles/QubeFS.dir/q_fuse.cpp.o"
+  "CMakeFiles/QubeFS.dir/qube.cpp.o"
+  "QubeFS"
+  "QubeFS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QubeFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
